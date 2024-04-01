9fe57b7b53f1e4f4db34164292fa12cfa1667d4e
helenm1 <95582910+helenm1@users.noreply.github.com>
2024-03-31 21:41:31 -0400

    adjusted listen app.js

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import express from 'express';
+import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
-// const express = require('express')
-const app = express()
-Hello(app);
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
 Lab5(app);
-app.listen(4000)
+Hello(app);
+app.listen(process.env.PORT || 4000);
