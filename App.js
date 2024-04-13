918f34f38726d4a5ad0df704d13d3d9f4fadf833
helenm1 <95582910+helenm1@users.noreply.github.com>
2024-04-13 17:13:50 -0400

    finished 3.5.2

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Kanbas/Users/routes.js";
 import cors from "cors";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,4 +14,5 @@ ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
