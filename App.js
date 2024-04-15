f9c10d1f05a615e3b246adb4622f1be74c7837e1
helenm1 <95582910+helenm1@users.noreply.github.com>
2024-04-14 20:43:22 -0400

    finished 4.4

--- App.js
+++ App.js
@@ -6,10 +6,34 @@ import ModuleRoutes from "./Kanbas/modules/routes.js";
 import mongoose from "mongoose";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import cors from "cors";
+import session from "express-session";
+import "dotenv/config";
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+
+app.use(session(sessionOptions));
+
 app.use(express.json());
+const port = process.env.PORT || 4000;
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
