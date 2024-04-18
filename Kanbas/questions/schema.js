1865eca9c681e3c3bfe24f3e6943b30be586c796
William Zhang <wzhang12321@gmail.com>
2024-04-18 16:17:51 -0400

    finished questions?

---
+++ Kanbas/questions/schema.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+const questionSchema = mongoose.Schema(
+  {
+    id: String,
+    quizId: { type: String, required: true },
+    questionText: { type: String, required: true },
+    points: { type: Number, required: true },
+    questionType: {
+      type: String,
+      enum: ["TRUE_FALSE", "MULTIPLE_CHOICE", "FILL_IN"],
+      default: "MULTIPLE_CHOICE",
+    },
+    tfAnswer: { type: Boolean, required: questionType === "TRUE_FALSE" },
+    mcAnswers: {
+      type: [
+        {
+          answer: String,
+          correct: Boolean,
+        },
+      ],
+      required: questionType === "MULTIPLE_CHOICE",
+    },
+    fillInAnswers: {
+      type: [String],
+      required: questionType === "FILL_IN",
+    },
+  },
+
+  { collection: "questions" }
+);
+export default questionSchema;
