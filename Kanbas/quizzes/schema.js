25e0971d7c6bc625c83b161c89468bce79a3d08b
William Zhang <wzhang12321@gmail.com>
2024-04-18 17:05:58 -0400

    added to quiz schema

--- Kanbas/quizzes/schema.js
+++ Kanbas/quizzes/schema.js
@@ -2,13 +2,41 @@ import mongoose from "mongoose";
 const quizzesSchema = mongoose.Schema(
   {
     id: String,
-    name: String,
-    availability: String,
-    published: Boolean,
-    dueDate: String,
-    points: Number,
-    numQuestions: Number,
-    course: String,
+    name: { type: String, required: true },
+    quizType: {
+      type: String,
+      enum: [
+        "GRADED_QUIZ",
+        "PRACTICE_QUIZ",
+        "GRADED_SURVEY",
+        "UNGRADED_SURVEY",
+      ],
+      default: "GRADED_QUIZ",
+    },
+    assignmentGroup: {
+      type: String,
+      enum: ["QUIZZES", "EXAMS", "ASSIGNMENTS", "PROJECT"],
+      default: "QUIZZES",
+    },
+    shuffleAnswers: { type: Boolean, default: true },
+    timeLimit: { type: Number, default: 20 },
+    multipleAttemps: { type: Boolean, default: false },
+    showCorrectAnswers: {
+      type: String,
+      enum: ["IMMEDIATELY", "AFTER_DUE_DATE", "NEVER"],
+      default: "NEVER",
+    },
+    accessCode: { type: String, default: "" },
+    oneQuestionAtATime: { type: Boolean, default: true },
+    webcamRequired: { type: Boolean, default: false },
+    lockQuestionsAfterAnswering: { type: Boolean, default: false },
+    published: { type: Boolean, default: false },
+    dueDate: { type: Date, required: true },
+    avaliableDate: { type: Date, required: true },
+    untilDate: { type: Date, required: true },
+    points: { type: Number, default: 0 },
+    numQuestions: { type: Number, default: 0 },
+    courseId: { type: String, required: true },
   },
   { collection: "quizzes" }
 );
